84d6904dd281fc3a5ca915f5383b6744cbaf6263
Paul W. Rankin <rnkn@rnkn.xyz>
2022-07-21 22:09:59 +1000

    Add todo.sh

--- todo.sh
+++ todo.sh
@@ -8,9 +8,8 @@ todo_file="${TODO_FILE:-${PWD}/TODO}"
 test -f "$todo_file" || fail "TODO_FILE not found"
 
 usage() {
-	echo "usage: $program [-e]"
-	echo "       $program [-d NUM]"
-	echo "       $program TASK"
+	echo "usage: $program TASK"
+	echo "       $program [-d LINENUM] [-e] [-Ss QUERY]"
 }
 
 # t_done(int)
@@ -18,21 +17,26 @@ t_done() {
 	int=$1
 	tmpfile=$(mktemp)
 	sed -n "${int}!p" "$todo_file" > "$tmpfile"
+	mv "$todo_file" "${todo_file}~"
 	mv "$tmpfile" "$todo_file"
 }
 
 t_print() {
 	lines=$(wc -l < "$todo_file")
 	width=$(echo $lines | wc -c)
-	nl -s' ' -w"${width}" "$todo_file"
+	nl -s' ' -w"$width" "$todo_file"
 }
 
 main() {
-	if getopts hed: opt; then
+	if getopts hed:S:s: opt; then
 		case "$opt" in
 			(e)	$EDITOR "$todo_file"
 				exit ;;
 			(d)	t_done "$OPTARG"
+				exit ;;
+			(S)	t_print | grep -iw "$OPTARG"
+				exit ;;
+			(s)	t_print | grep -i "$OPTARG"
 				exit ;;
 			(h)	usage
 				exit ;;
